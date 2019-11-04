d63609a3fd9424f18dd2e389019091d8d5abba02
Administrator <Administrator@SC-201907201119>
2019-11-04 22:20:40 +0800

    init

--- easywebsocket-client/src/main/java/org/easymis/easywebsocket/client/config/WebSocketConfig.java
+++ easywebsocket-client/src/main/java/org/easymis/easywebsocket/client/config/WebSocketConfig.java
@@ -1,17 +1,51 @@
 package org.easymis.easywebsocket.client.config;
 
+import java.net.URI;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import lombok.extern.slf4j.Slf4j;
 /**
  * 
  * @author 谭宇杰
  *
  */
+@Slf4j
 @Component
-public class WebSocketConfig {
-	@Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+public class WebSocketConfig { @Bean
+    public WebSocketClient webSocketClient() {
+    try {
+        WebSocketClient webSocketClient = new WebSocketClient(new URI("ws://localhost:8085/websocket/test"),new Draft_6455()) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                log.info("[websocket] 连接成功");
+            }
+
+            @Override
+            public void onMessage(String message) {
+                log.info("[websocket] 收到消息={}",message);
+
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                log.info("[websocket] 退出连接");
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                log.info("[websocket] 连接错误={}",ex.getMessage());
+            }
+        };
+        webSocketClient.connect();
+        return webSocketClient;
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+    return null;
+}
 }
--- easywebsocket-server/src/main/java/org/easymis/easywebsocket/server/EasyWebSocketApplication.java
+++ easywebsocket-server/src/main/java/org/easymis/easywebsocket/server/EasyWebSocketApplication.java
@@ -3,25 +3,16 @@ package org.easymis.easywebsocket.server;
 
 
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@ComponentScan(basePackages = "org.easymis.easycompany")
-@EnableTransactionManagement(order = 2) // 设置事务执行顺序(需要在切换数据源之后，否则只走主库)
-@MapperScan(basePackages = "org.easymis.easycompany.entitys.mybatis.mapper")
-@ServletComponentScan
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds =  60*60*8 )
+@SpringBootApplication
+@ComponentScan(basePackages = " org.easymis.easywebsocket.server")
 @EnableSwagger2
 public class EasyWebSocketApplication {
 	protected static final Logger logger = LoggerFactory.getLogger(EasyWebSocketApplication.class);
